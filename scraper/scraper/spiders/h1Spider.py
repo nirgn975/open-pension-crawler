087cb764d259b384b59fba81904bef70d3499eba
Daniel <danellevi11234567@gmail.com>
2016-10-19 11:56:44 +0300

    Add Spider

--- scraper/scraper/spiders/h1Spider.py
+++ scraper/scraper/spiders/h1Spider.py
@@ -2,21 +2,15 @@ from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 import scrapy
 
-
 class QuotesSpider(scrapy.Spider):
     name = "h1"
-
     def start_requests(self):
         urls = [
             'https://www.yl-invest.co.il/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-
     def parse(self, response):
         filename = "h1 headers"
         with open(filename, 'wb') as f:
             f.write(response.xpath("//h1/text()")[0].extract())
-
-
-
