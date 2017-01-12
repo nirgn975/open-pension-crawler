651167266a981cbbc523699a2482d8de6414f022
Nir Galon <nir@galons.io>
2017-01-12 11:11:31 +0200

    Finish crawler for yl (#16)
    * Finish yl crawler
    
    * Add gitignore and travis-ci files
    
    * Cleanup
    
    * Fix travis-ci and add pyup badges
    
    * Fix pycodestyle exclude path

--- open_pension_crawler/pipelines.py
+++ open_pension_crawler/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ScraperPipeline(object):
+class OpenPensionCrawlerPipeline(object):
     def process_item(self, item, spider):
         return item
