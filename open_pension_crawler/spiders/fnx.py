ab36a8d41bf805e31b57c79145d5493a0f6bfc95
RoySegall <roy.segall@gizra.com>
2017-10-16 19:24:14 +0300

    Fix CS.

--- open_pension_crawler/spiders/fnx.py
+++ open_pension_crawler/spiders/fnx.py
@@ -4,5 +4,6 @@ from open_pension_crawler.OpenPensionCrawlSpiderBase import OpenPensionCrawlSpid
 class FnxSpider(OpenPensionCrawlSpiderBase):
     name = 'fnx'
     allowed_domains = ['www.fnx.co.il']
-    start_urls = ['https://www.fnx.co.il/pension-savings/pension/phoenix-pension-providence-ltd/quarterly-assets-list.aspx']
+    start_urls = ['https://www.fnx.co.il/pension-savings/pension/phoenix-pension-providence-ltd/'
+                  'quarterly-assets-list.aspx']
     regex = r'(.*)(01|02|03|04)[0-9]{2}.(xlsx|xls)'
